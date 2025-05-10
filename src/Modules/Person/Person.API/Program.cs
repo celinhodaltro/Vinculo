1a3a70ff1759fd15334d14844044880ab4004505
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2025-05-10 01:31:27 -0300

    Ajustes na construção do aplicativo e configurações
    Inclui a construção do aplicativo em `Program.cs` com a linha `var app = builder.Build();`.
    No `launchSettings.json`, reorganiza a estrutura do JSON, adiciona o perfil "WSL" e remove propriedades duplicadas ou desnecessárias.

--- src/Modules/Person/Person.API/Program.cs
+++ src/Modules/Person/Person.API/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
