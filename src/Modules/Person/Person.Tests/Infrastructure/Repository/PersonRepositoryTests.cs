bb1f348c0d252cf79f762b91b87672c0c0b3ad34
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-05-13 13:52:17 -0300

    Adiciona testes para PersonRepository
    - Criada a classe `PersonRepositoryTests` no namespace `Person.Tests.Infrastructure`.
    - Configurado contexto de banco de dados em memória para testes.
    - Implementado teste `AddAsync_ShouldAddPersonToDatabase`:
      - Verifica se um objeto `User` é adicionado corretamente ao banco.
      - Valida os dados do usuário adicionado, como nome "Ana".

---
+++ src/Modules/Person/Person.Tests/Infrastructure/Repository/PersonRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Person.Domain.Entities;
+using Person.Infrastructure.Persistence;
+using Person.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Person.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace Person.Tests.Infrastructure
+{
+    public class PersonRepositoryTests
+    {
+        private readonly AppDbContext _context;
+        private readonly PersonRepository _repository;
+
+        public PersonRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+
+            _context = new AppDbContext(options);
+            _repository = new PersonRepository(_context);
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldAddPersonToDatabase()
+        {
+            // Arrange
+            var person = new User("Ana", new Cpf("000.000.000-00"), 22);
+
+            // Act
+            await _repository.AddAsync(person);
+            var result = await _context.Users.FirstOrDefaultAsync();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be("Ana");
+        }
+    }
+
+}
