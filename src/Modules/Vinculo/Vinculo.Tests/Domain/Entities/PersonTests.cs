81bbc6b1012c177cb9c9404f85cc629e00455bc2
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-05-14 08:52:38 -0300

    Corrige namespace e verificações em PersonTests.cs
    O namespace foi ajustado no arquivo `PersonTests.cs` para corrigir
    o uso da classe `Person`, removendo o prefixo incorreto `Vinculo`.
    As verificações das propriedades (`Name`, `Age` e `Cpf`) foram
    atualizadas para usar o objeto `person` diretamente.

--- src/Modules/Vinculo/Vinculo.Tests/Domain/Entities/PersonTests.cs
+++ src/Modules/Vinculo/Vinculo.Tests/Domain/Entities/PersonTests.cs
@@ -9,10 +9,10 @@ namespace Vinculo.Tests.Domain.Entities
         [Fact]
         public void Constructor_ShouldSetPropertiesCorrectly()
         {
-            var person = new Vinculo.Person("Maria", new Cpf("000.000.000-00") , 25);
-            Vinculo.Name.Should().Be("Maria");
-            Vinculo.Age.Should().Be(25);
-            Vinculo.Cpf.Should().Be("000.000.000-00");
+            var person = new Person("Maria", new Cpf("000.000.000-00") , 25);
+            person.Name.Should().Be("Maria");
+            person.Age.Should().Be(25);
+            person.Cpf.Should().Be("000.000.000-00");
         }
     }
 }
