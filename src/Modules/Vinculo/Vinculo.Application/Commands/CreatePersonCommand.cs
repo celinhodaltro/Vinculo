40ba09dcc9664659dad77cd0d48cb8ec2c39afba
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-05-14 09:14:27 -0300

    Remoção de imports desnecessários em PersonRepositoryTests
    Foram removidas as bibliotecas `System`, `System.Collections.Generic`,
    `System.Linq`, `System.Text` e `System.Threading.Tasks` do arquivo
    `PersonRepositoryTests.cs`. Essa limpeza melhora a legibilidade e
    manutenção do código, eliminando dependências não utilizadas.

--- src/Modules/Vinculo/Vinculo.Application/Commands/CreatePersonCommand.cs
+++ src/Modules/Vinculo/Vinculo.Application/Commands/CreatePersonCommand.cs
@@ -4,8 +4,8 @@ namespace Vinculo.Application.Commands
 {
     public class CreatePersonCommand : IRequest<Guid>
     {
-        public string Name { get; set; }
-        public string Cpf { get; set; }
+        public string? Name { get; set; }
+        public string? Cpf { get; set; }
         public int Age { get; set; }
     }
 }
--- src/Modules/Vinculo/Vinculo.Tests/Infrastructure/Repository/PersonRepositoryTests.cs
+++ src/Modules/Vinculo/Vinculo.Tests/Infrastructure/Repository/PersonRepositoryTests.cs
@@ -1,11 +1,6 @@
 ﻿using Vinculo.Domain.Entities;
 using Vinculo.Infrastructure.Persistence;
 using Vinculo.Infrastructure.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vinculo.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using FluentAssertions;
