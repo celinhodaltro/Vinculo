e528050bde4e8c8275063d8f8ab2935a7b12d963
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-06-09 16:36:03 -0300

    ajustar projeto para separar auth

--- src/Modules/Vinculo/Vinculo.Infrastructure/Configurations/PersonConfiguration.cs
+++ src/Modules/Vinculo/Vinculo.Infrastructure/Configurations/PersonConfiguration.cs
@@ -4,7 +4,7 @@ using Vinculo.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text;.
 using System.Threading.Tasks;
 
 namespace Vinculo.Infrastructure.Configurations
